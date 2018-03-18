ef6984673a538156466e0f4087f901d53e33446e
Sean Hull <hullsean@gmail.com>
2018-03-18 11:40:52 -0400

    adding identidock unit tests

--- app/identidock.py
+++ app/identidock.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
+import html
 
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port='6379', db=0)
@@ -12,7 +13,9 @@ default_name = 'Sean Hull'
 def mainpage():
     name = default_name
     if request.method == 'POST':
-        name = request.form['name']
+#        name = request.form['name']
+# sanitizing user input
+        name = html.escape(request.form['name'], quote=True)
 
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
@@ -32,7 +35,9 @@ def mainpage():
 @app.route('/monster/<name>')
 def get_identicon(name):
 
+    name = html.escape(name, quote=True)
     image = cache.get(name)
+
     if image is None:
         print ("Cache miss", flush=True)
         r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
